cf248b7e923f2c9862ccd9f2843603f2dd827c6e
Willian <williandeitosi@gmail.com>
2023-11-12 13:18:04 -0300

    att

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conectionDB = () => {
   mongoose.connect(
-    `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORLD_DB}@api-livros.pmjxeyj.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@api-livros.pmjxeyj.mongodb.net/?retryWrites=true&w=majority`
   );
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "Erro de conexão com MongoDB"));
